28357e715b4fc3aaa0f7bd36ce14f0be100b7753
¨David¨ <daviddejesushh@gmail.com>
2026-02-13 09:55:23 -0600

    aaaa

---
+++ sistema-cable/src/app/admin/gestion-pagos/gestion-pagos.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gestion-pagos',
+  imports: [],
+  templateUrl: './gestion-pagos.html',
+  styleUrl: './gestion-pagos.css',
+})
+export class GestionPagos {
+
+}
---
+++ sistema-cable/src/app/cliente/home-cliente/home-cliente.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-cliente',
+  imports: [],
+  templateUrl: './home-cliente.html',
+  styleUrl: './home-cliente.css',
+})
+export class HomeCliente {
+
+}
